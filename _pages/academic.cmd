62e879b7b9acf6757d149da0ec600c834292fbc2
Dipen Shome <157332326+dipenshome@users.noreply.github.com>
2024-09-18 21:13:17 +0600

    update

---
+++ _pages/academic.cmd
@@ -0,0 +1,9 @@
+---
+layout: archive
+title: "Academic Info :mortar_board:"
+permalink: /academic/
+author_profile: true
+---
+## Bachelor of Science in Mechanical Engineering
+### Bangladesh University of Engineering & Technology 
+##
